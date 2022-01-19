34586776e6587c914475971404af4a2334620d88
Jerry Tong <zihangtong@gmail.com>
2022-01-18 19:59:53 -0500

    Finished

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.pluck(:item_name).include?(item_name)
+    end
+
+    def give_away(dev, freebie)
+        if self.freebies.include?(freebie)
+            freebie.dev = dev
+            freebie.save
+            "#{self.name} gave #{dev.name} a #{freebie.item_name}"
+        else "#{self.name} doesn't own a #{freebie.item_name}!"
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,7 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Sweatshirt", value: 30, company: c1, dev: d3)
+Freebie.create(item_name: "Hat", value: 15, company: c2, dev: d1)
 
 puts "Seeding done!"
